0c8be4ce5d0d8ab4fa3df5dfbe5a5ff7393b2fab
Jurgene <Jurgene@JurgenE-LT.dm.cx>
2022-06-17 01:14:09 +0300

    first commit

---
+++ src/main/java/net/javaguides/springboot/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.service;
+
+import java.util.List;
+
+import net.javaguides.springboot.model.Employee;
+
+
+public interface EmployeeService {
+	List <Employee> getAllEmployees();
+	void saveEmployee(Employee employee);
+	Employee getEmployeeByid(long id);
+	void deleteEmployeeById(long id);
+}
